bc1c310a69811085b836a9ae766b83d9206eee67
Damien McDonnell <dm@cct-marketing.com>
2020-12-18 06:58:05 +0000

    Added TailWindCSS and Nav component for testing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="flex items-center justify-center flex-nowrap bg-teal-500 p-6">
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav />
+    <HelloWorld msg="Welcome to Your Vue.js App" class="text-center"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Nav from '@/components/Nav.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Nav
   }
 }
 </script>
