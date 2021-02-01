277c6f85b245241a074b9aef6648f4cce55a3d88
Damien McDonnell <damien@friday.ie>
2021-02-01 12:07:46 +0000

    Fixed type issue with deadline_passed parameter in get all game picks

--- api/get-all-current-game-picks.php
+++ api/get-all-current-game-picks.php
@@ -4,18 +4,23 @@ header('Access-Control-Allow-Origin: *');
 require 'includes/db-connect.php';
 
 if (isset($_POST['game_id']) and isset($_POST['user_role']) and isset($_POST['user_id']) and isset($_POST['deadline_passed'])) {
+    $response = array();
+    $players_picks = array();
+
     $game_id = $_POST['game_id'];
     $user_role = $_POST['user_role'];
     $user_id = $_POST['user_id'];
     $deadline_passed = $_POST['deadline_passed'];
 
-    if ($user_role == 'admin' or $deadline_passed == true) {
+    if ($user_role == 'admin' or $deadline_passed == 'true') {
+        // $response['query_type'] = 'admin or deadline passed';
         $query = "SELECT tbl_player_picks.picks_id AS picks_id, tbl_player_picks.picks_user_id AS picks_user_id, tbl_users.user_firstname AS user_firstname, tbl_users.user_lastname AS user_lastname, tbl_player_picks.picks_player_1 AS picks_player_1, tbl_player_picks.picks_player_2 AS picks_player_2, tbl_player_picks.picks_player_3 AS picks_player_3, tbl_player_picks.picks_player_4 AS picks_player_4, tbl_player_picks.picks_player_5 AS picks_player_5
         FROM tbl_player_picks
         LEFT JOIN tbl_users
         ON tbl_player_picks.picks_user_id = tbl_users.user_id
         WHERE tbl_player_picks.picks_game_id = '$game_id'";
     } else {
+        // $response['query_type'] = 'NOT admin and deadline NOT passed';
         $query = "SELECT tbl_player_picks.picks_id AS picks_id, tbl_player_picks.picks_user_id AS picks_user_id, tbl_users.user_firstname AS user_firstname, tbl_users.user_lastname AS user_lastname, tbl_player_picks.picks_player_1 AS picks_player_1, tbl_player_picks.picks_player_2 AS picks_player_2, tbl_player_picks.picks_player_3 AS picks_player_3, tbl_player_picks.picks_player_4 AS picks_player_4, tbl_player_picks.picks_player_5 AS picks_player_5
         FROM tbl_player_picks
         LEFT JOIN tbl_users
@@ -27,8 +32,10 @@ if (isset($_POST['game_id']) and isset($_POST['user_role']) and isset($_POST['us
     $results = mysqli_query($conn, $query) or die(mysqli_error($conn));
     $count = mysqli_num_rows($results);
 
-    $response = array();
-    $players_picks = array();
+    // $response['game_id'] = $game_id;
+    // $response['user_role'] = $user_role;
+    // $response['user_id'] = $user_id;
+    // $response['deadline_passed'] = $deadline_passed;
 
     if ($count > 0) {
         while ($result = mysqli_fetch_object($results)) {
